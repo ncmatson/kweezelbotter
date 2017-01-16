17a2488048d35e38f8ca26eeaae3dcb1b18e4750
claytongentry <claytongentry2017@u.northwestern.edu>
2017-01-16 00:09:41 -0600

    fill out test dict

--- kweezelbotter/kweezelbotter.py
+++ kweezelbotter/kweezelbotter.py
@@ -2,10 +2,26 @@ class Kweezelbotter:
 
     def __init__(self, word):
         self.dictionary = "./dict/dict.txt"
-        self.word = word
+        self.word       = word
+        self.phoneme    = []
+
+    def fetch_phoneme(self):
+        self.phoneme
+
+    def get_phoneme(self):
+        return self.phoneme
+
+    def set_phoneme(self, phoneme):
+        self.phoneme = phoneme
 
     def get_dictionary(self):
         return self.dictionary
 
     def set_dictionary(self, dict_path):
         self.dictionary = dict_path
+
+    def get_word(self):
+        return self.word
+
+    def set_word(self, word):
+        self.word = word
