97fc0692845e2932e68c0c6881031c90994e0fb6
Cameron Matson <cmatson@smu.edu>
2017-01-16 22:58:32 -0600

    add setup to subset test

--- test/subset_test.py
+++ test/subset_test.py
@@ -3,23 +3,22 @@ import unittest
 from subset import Subset
 
 class SubsetUnitTestCase(unittest.TestCase):
+    def setUp(self):
+       self.s = Subset('deer')
 
     def test_inits_word(self):
-        s = Subset('deer')
-        self.assertEqual(s.word, 'deer')
+        self.assertEqual(self.s.word, 'deer')
 
     def test_calculate_max_chunks(self):
-        s = Subset('deer')
 
-        scores = s.calculateChunkPatterns()
+        self.s.calculateChunkPatterns()
 
-        self.assertEqual(scores, [[2,2], [4]])
+        self.assertEqual(self.s.patterns, [[2,2], [4]])
 
     def test_chunk_given_patterns(self):
         patterns = [[2,2], [4]]
-        s = Subset('deer')
 
-        chunks = s.applyPatterns(patterns)
+        chunks = self.s.applyPatterns(patterns)
 
         self.assertEqual(chunks, [['de', 'er'], ['deer']])
 
